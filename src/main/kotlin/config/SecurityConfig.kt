f39d98156996e716d956744686d0c5390a0cf93c
pelin0866 <pelin0866@gmail.com>
2025-10-31 12:13:50 +0100

    init: feat: Simplify JWT Operation

--- src/main/kotlin/config/SecurityConfig.kt
+++ src/main/kotlin/config/SecurityConfig.kt
@@ -13,30 +13,46 @@ import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig(
-    private val jwtFilter: JwtAuthFilter,
-    private val userDetailsService: UserDetailsServiceImpl
-) {
-
-    @Bean fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+class SecurityConfig {
 
     @Bean
-    fun authenticationManager(): AuthenticationManager {
-        val p = DaoAuthenticationProvider()
-        p.setUserDetailsService(userDetailsService)
-        p.setPasswordEncoder(passwordEncoder())
-        return ProviderManager(p)
-    }
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain =
-        http.csrf { it.disable() }
-            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+        http
+            .csrf { it.disable() }                 // disable CSRF protection
             .authorizeHttpRequests {
-                it.requestMatchers("/auth/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                it.requestMatchers("/users/**").hasAnyRole("USER","ADMIN")
-                it.anyRequest().authenticated()
+                it.anyRequest().permitAll()        // For all endpoints = permilAll
             }
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
 }
+
+//@Configuration
+//class SecurityConfig(
+//    private val jwtFilter: JwtAuthFilter,
+//    private val userDetailsService: UserDetailsServiceImpl
+//) {
+//
+//    @Bean fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+//
+//    @Bean
+//    fun authenticationManager(): AuthenticationManager {
+//        val p = DaoAuthenticationProvider()
+//        p.setUserDetailsService(userDetailsService)
+//        p.setPasswordEncoder(passwordEncoder())
+//        return ProviderManager(p)
+//    }
+//
+//    @Bean
+//    fun filterChain(http: HttpSecurity): SecurityFilterChain =
+//        http.csrf { it.disable() }
+//            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+//            .authorizeHttpRequests {
+//                it.requestMatchers("/auth/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+//                it.requestMatchers("/users/**").hasAnyRole("USER","ADMIN")
+//                it.anyRequest().authenticated()
+//            }
+//            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+//            .build()
+//}
